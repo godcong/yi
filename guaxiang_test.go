bd4348f42481d19d319968e6190a766f916c8a17
godcong <jumbycc@163.com>
2018-09-23 01:10:56 +0800

    add yao

--- guaxiang_test.go
+++ guaxiang_test.go
@@ -2,8 +2,7 @@ package core
 
 import "testing"
 
-func TestSetGuaXiang(t *testing.T) {
-	gx := make(map[string]*GuaXiang)
+func qian(gx map[string]*GuaXiang) map[string]*GuaXiang {
 	gx["乾坤"] = &GuaXiang{
 		ShangGua: "乾为天",
 		ShangShu: 0x00,
@@ -87,6 +86,11 @@ func TestSetGuaXiang(t *testing.T) {
 		YongJiXiong:     "-",
 	}
 
+	return gx
+}
+
+func kun(gx map[string]*GuaXiang) map[string]*GuaXiang {
+
 	gx["坤坤"] = &GuaXiang{
 		ShangGua: "坤为地",
 		ShangShu: 0x07,
@@ -147,7 +151,10 @@ func TestSetGuaXiang(t *testing.T) {
 		XiaShu:   0x00,
 		GuaMing:  "地天泰",
 	}
+	return gx
+}
 
+func gen(gx map[string]*GuaXiang) map[string]*GuaXiang {
 	gx["艮坤"] = &GuaXiang{
 		ShangGua: "艮为山",
 		ShangShu: 0x06,
@@ -207,6 +214,72 @@ func TestSetGuaXiang(t *testing.T) {
 		XiaGua:   "乾为天",
 		XiaShu:   0x00,
 		GuaMing:  "山天大畜",
+	}
+
+	gx["坎坤"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "坤为地",
+		XiaShu:   0x07,
+		GuaMing:  "水地比",
+	}
+	return gx
+}
+
+func TestSetGuaXiang(t *testing.T) {
+	gx := make(map[string]*GuaXiang)
+
+	gx["坎艮"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "艮为山",
+		XiaShu:   0x06,
+		GuaMing:  "水山蹇",
+	}
+
+	gx["坎坎"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "坎为水",
+		XiaShu:   0x05,
+		GuaMing:  "坎为水",
+	}
+	gx["坎巽"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "巽为风",
+		XiaShu:   0x04,
+		GuaMing:  "水风井",
+	}
+	gx["坎震"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "震为雷",
+		XiaShu:   0x03,
+		GuaMing:  "水雷屯",
+	}
+
+	gx["坎离"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "离为火",
+		XiaShu:   0x02,
+		GuaMing:  "水火既济",
+	}
+
+	gx["坎兑"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "兑为泽",
+		XiaShu:   0x01,
+		GuaMing:  "水泽节",
+	}
+	gx["坎乾"] = &GuaXiang{
+		ShangGua: "坎为水",
+		ShangShu: 0x05,
+		XiaGua:   "乾为天",
+		XiaShu:   0x00,
+		GuaMing:  "水天需",
 	}
 
 	gx["坎坤"] = &GuaXiang{
