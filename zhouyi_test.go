32e34ee217837bd00cbd3900bd9bf6c6da9b09da
godcong <jumbycc@163.com>
2018-09-29 22:41:15 +0800

    delete test guaxiang

--- zhouyi_test.go
+++ zhouyi_test.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"log"
 	"testing"
 	"time"
 )
@@ -43,18 +42,18 @@ func TestTimeToBian(t *testing.T) {
 
 func TestNumberQiGua(t *testing.T) {
 	//t.Log(bian(7, 0))
-	gua1 := NumberQiGua(23, 20, int(StringToTime("2018-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua1.Get(BenGua), gua1.Get(BianGua))
-	gua2 := NumberQiGua(23, 20, int(StringToTime("2019-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua2.Get(BenGua), gua2.Get(BianGua))
-	gua3 := NumberQiGua(23, 20, int(StringToTime("2020-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua3.Get(BenGua), gua3.Get(BianGua))
-	gua4 := NumberQiGua(23, 20, int(StringToTime("2021-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua4.Get(BenGua), gua4.Get(BianGua))
-	gua5 := NumberQiGua(23, 20, int(StringToTime("2022-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua5.Get(BenGua), gua5.Get(BianGua))
-	gua6 := NumberQiGua(23, 20, int(StringToTime("2023-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua6.Get(BenGua), gua6.Get(BianGua))
-	gua7 := NumberQiGua(23, 20, int(StringToTime("2024-01-01 00:00").Unix()/1000000%6))
-	log.Println(gua7.Get(BenGua), gua7.Get(BianGua))
+	//gua1 := NumberQiGua(23, 20, int(StringToTime("2018-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua1.Get(BenGua), gua1.Get(BianGua))
+	//gua2 := NumberQiGua(23, 20, int(StringToTime("2019-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua2.Get(BenGua), gua2.Get(BianGua))
+	//gua3 := NumberQiGua(23, 20, int(StringToTime("2020-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua3.Get(BenGua), gua3.Get(BianGua))
+	//gua4 := NumberQiGua(23, 20, int(StringToTime("2021-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua4.Get(BenGua), gua4.Get(BianGua))
+	//gua5 := NumberQiGua(23, 20, int(StringToTime("2022-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua5.Get(BenGua), gua5.Get(BianGua))
+	//gua6 := NumberQiGua(23, 20, int(StringToTime("2023-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua6.Get(BenGua), gua6.Get(BianGua))
+	//gua7 := NumberQiGua(23, 20, int(StringToTime("2024-01-01 00:00").Unix()/1000000%6))
+	//log.Println(gua7.Get(BenGua), gua7.Get(BianGua))
 }
