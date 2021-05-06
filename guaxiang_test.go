e5849c2f1fad37fc5cd234e2602945a4d8784089
godcong <jumbycc@163.com>
2021-05-06 12:04:38 +0800

    update 1. fix go.mod

--- guaxiang_test.go
+++ guaxiang_test.go
@@ -4,9 +4,50 @@ import (
 	"testing"
 )
 
-func TestGetGuaXiang(t *testing.T) {
-	if len(getGuaXiangs()) != 64 {
-		t.Log("not enough", getGuaXiangs())
+func TestGetGuaXiang1(t *testing.T) {
+	gx := getGuaXiangs()
+	if len(gx) != 64 {
+		t.Fatal("not enough", len(gx))
 	}
+	for i := range gx {
+		t.Logf("idx(%+v):%+v", i, gx[i])
+	}
+}
 
+func TestGetGuaXiang(t *testing.T) {
+	type args struct {
+		guaIdx string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *GuaXiang
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				guaIdx: "兑艮",
+			},
+			want: &GuaXiang{
+				GuaXu:    31,
+				ShangShu: 1,
+				XiaShu:   6,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				got := GetGuaXiang(tt.args.guaIdx)
+				t.Logf("卦象：%+v", got)
+				if got.ShangShu != tt.want.ShangShu {
+					t.Errorf("GetGuaXiang() = %v, want %v", got, tt.want)
+				}
+				if got.XiaShu != tt.want.XiaShu {
+					t.Errorf("GetGuaXiang() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
 }
